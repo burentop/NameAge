dede72f9882a3236b56791f4e1bac1040376104f
Brent Perry <burentoperii@gmail.com>
2016-07-11 08:35:10 -0500

    Initial commit of NameAge

--- NameAge/main.cpp
+++ NameAge/main.cpp
@@ -6,10 +6,33 @@
 //  Copyright © 2016 Brent Perry. All rights reserved.
 //
 
-#include <iostream>
+#include "../../../std_lib_facilities.h"
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+int main()
+{
+    vector<string> names;
+    vector<int> ages;
+    string tempname = "A";
+    int tempage = 1;
+    
+    while (tempname != "NoName" && tempage != 0) {
+        cout << "Please enter a name and an age (separated by a space).\n";
+        cout << "(Enter 'NoName 0' to quit.\n";
+        cin >> tempname >> tempage;
+        for (string name : names) {
+            if (tempname == name) {
+                tempname = "NoName";
+                tempage = 0;
+                break;
+            }
+        }
+        names.push_back(tempname);
+        ages.push_back(tempage);
+    }
+    
+    cout << "You entered: \n";
+    for (int i = 0; i < names.size(); ++i) {
+        cout << names[i] << " " << ages[i] << "\n";
+    }
+    
 }
