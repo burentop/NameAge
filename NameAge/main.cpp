f7446342aafc19872ae70f28ad19ac82d571db9a
Brent Perry <burentoperii@gmail.com>
2016-07-11 08:22:28 -0500

    Initial Commit

---
+++ NameAge/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  NameAge
+//
+//  Created by Brent Perry on 7/11/16.
+//  Copyright © 2016 Brent Perry. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
